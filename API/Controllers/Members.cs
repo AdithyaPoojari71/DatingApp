cac045e44b3d445056130a643d4cf66daee3a4c6
AdithyaPoojari71 <adityapoojari71@gmail.com>
2025-07-30 10:08:03 +0530

    section 5 added.

--- API/Controllers/Members.cs
+++ API/Controllers/Members.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [Authorize]
+    //[Authorize]
     public class MembersController : BaseApiController
     {
         private readonly AppDbContext _context;
